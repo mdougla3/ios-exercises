3d65106762594d7b55d525d98c788148153e91e7
McCay <useruser@users-MacBook.attlocal.net>
2015-06-05 16:08:00 -0500

    Assignment 9 complete

--- BlocExercises/BlocExercises/EqualityDeterminer.m
+++ BlocExercises/BlocExercises/EqualityDeterminer.m
@@ -11,18 +11,18 @@
 @implementation EqualityDeterminer
 
 - (BOOL) string:(NSString *)string1 isTheSameAsString:(NSString *)string2 {
-    /* WORK HERE */
-    return (string1 == string2);
+        BOOL cerealEquality = [string1 isEqualToString:string2];
+        return cerealEquality;
 }
 
 - (BOOL) number:(NSNumber *)number1 isTheSameAsNumber:(NSNumber *)number2 {
-    /* WORK HERE */
-    return (number1 == number2);
+        BOOL numberEquality = [number1 isEqualToNumber:number2];
+        return numberEquality;
 }
 
 - (BOOL) integer:(NSInteger)integer1 isGreaterThan:(NSInteger)integer2 {
-    /* WORK HERE */
-    return NO;
+    BOOL isGreaterThan = integer1 > integer2;
+    return isGreaterThan;
 }
 
 @end
