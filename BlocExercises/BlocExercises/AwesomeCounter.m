efc974ebbe91838973fd3d4303a8160b79588098
McCay <useruser@users-MacBook.attlocal.net>
2015-06-06 23:50:04 -0500

    Assignment loops complete

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -11,8 +11,26 @@
 @implementation AwesomeCounter
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @"";
+    
+    NSString *newValue = @"";
+    
+    if (number < otherNumber) {
+        while (number <= otherNumber) {
+            NSString *readOut = [NSString stringWithFormat:@"%ld", number];
+            number++;
+            newValue = [newValue stringByAppendingString:readOut];
+        }
+    }
+    else if (number > otherNumber){
+        while (otherNumber <= number) {
+            NSString *readOut = [NSString stringWithFormat:@"%ld", otherNumber];
+            otherNumber++;
+            newValue = [newValue stringByAppendingString:readOut];
+    }
 }
-
+    else {
+        return newValue = [NSString stringWithFormat:@"%ld", number];
+    }
+    return newValue;
+}
 @end
