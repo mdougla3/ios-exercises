39ff74b87d902fae2e9098eb956d0c5977a82ad2
McCay <useruser@users-MacBook.attlocal.net>
2015-06-16 09:43:52 -0500

    Done

--- Swift vs.playground/Contents.swift
+++ Swift vs.playground/Contents.swift
@@ -1,5 +1,108 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-var str = "Hello, playground"
+/*
+
+Strings
+
+*/
+
+func favoriteCheeseStringWithCheese(cheese: String) -> String {
+    
+    return "My favorite cheese is \(cheese)"
+}
+
+var fullSentence = favoriteCheeseStringWithCheese("cheddar")
+// Make fullSentence say "My favorite cheese is cheddar."
+
+fullSentence
+
+/*
+
+Arrays & Dictionaries
+
+*/
+
+var numberArray = [1, 2, 3, 4]
+// Add 5 to this array
+numberArray.append(5)
+
+
+var numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
+// Add 5 : "five" to this dictionary
+numberDictionary[5] = "five"
+numberDictionary
+
+//
+/*
+
+Loops
+
+*/
+
+// Use a closed range loop to print 1 - 10, inclusively
+for i in 1...10 {
+    println(i)
+}
+
+// Use a half-closed range loop to print 1 - 10, inclusively
+for i in 1..<11 {
+    println(i)
+}
+
+let worf = [
+    "name": "Worf",
+    "rank": "lieutenant",
+    "information": "son of Mogh, slayer of Gowron",
+    "favorite drink": "prune juice",
+    "quote" : "Today is a good day to die."]
+
+let picard = [
+    "name": "Jean-Luc Picard",
+    "rank": "captain",
+    "information": "Captain of the USS Enterprise",
+    "favorite drink": "tea, Earl Grey, hot"]
+
+let characters = [worf, picard]
+
+func favoriteDrinksArrayForCharacters(characters:Array<Dictionary<String, String>>) -> Array<String> {
+    // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
+    var newArray = [String]()
+    
+    for (names) in characters {
+        newArray += [names["favorite drink"]!]
+    }
+    
+    return newArray
+}
+
+let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
+
+favoriteDrinks
+
+/*
+
+Functions
+
+*/
+
+// Make a function that inputs an array of strings and outputs the strings separated by a semicolon
+
+let strings = ["milk", "eggs", "bread", "challah"]
+
+// WORK HERE - make your function and pass `strings` in
+
+var expectedOutput = ";" .join(strings)
+
+
+//let expectedOutput = "milk;eggs;bread;challah"
+
+/*
+
+Closures
+
+*/
+
+let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All Berries", "Cookie Crisp"]
+
+let sortedCerealArray = sorted(cerealArray, <)
+
